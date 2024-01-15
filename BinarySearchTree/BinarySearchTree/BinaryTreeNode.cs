03ef955f70651bf3e48a0d7ef84f93124a43aee3
donatellony <donatellony4736@gmail.com>
2024-01-15 20:05:23 +0100

    Added the Binary Tree with Insert and Lookup (And Lookup Iterative) methods

---
+++ BinarySearchTree/BinarySearchTree/BinaryTreeNode.cs
@@ -0,0 +1,88 @@
+﻿using System.Text.Json;
+
+namespace BinarySearchTree;
+
+public class BinaryTreeNode(int value)
+{
+    public int Value { get; init; } = value;
+    public BinaryTreeNode? Left { get; set; }
+    public BinaryTreeNode? Right { get; set; }
+
+    private static readonly JsonSerializerOptions SerializerOptions = new()
+    {
+        WriteIndented = true
+    };
+
+    public void Insert(int value)
+    {
+        if (value < Value)
+        {
+            if (Left is null)
+            {
+                Left = new BinaryTreeNode(value);
+            }
+            else
+            {
+                Left.Insert(value);
+            }
+        }
+        else
+        {
+            if (Right is null)
+            {
+                Right = new BinaryTreeNode(value);
+            }
+            else
+            {
+                Right.Insert(value);
+            }
+        }
+    }
+
+    public bool Lookup(int value)
+    {
+        if (value == Value)
+            return true;
+
+        if (value < Value)
+        {
+            return Left is not null && Left.Lookup(value);
+        }
+
+        return Right is not null && Right.Lookup(value);
+    }
+
+    public bool LookupIterative(int value)
+    {
+        int currentNodeValue;
+        var currentNode = this;
+        do
+        {
+            currentNodeValue = currentNode.Value;
+            if (value == currentNodeValue)
+                return true;
+
+            var leftNode = currentNode.Left;
+            var rightNode = currentNode.Right;
+            if (value < currentNodeValue)
+            {
+                if (leftNode is null)
+                    return false;
+                currentNode = leftNode;
+            }
+            else
+            {
+                if (rightNode is null)
+                    return false;
+                currentNode = rightNode;
+            }
+        } while (value != currentNodeValue);
+
+        return false; // Should not happen
+    }
+
+    public string GetFormattedJson()
+    {
+        return JsonSerializer.Serialize(this, SerializerOptions);
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using BinarySearchTree;
+
+var tree = new BinaryTreeNode(9);
+tree.Insert(4);
+tree.Insert(20);
+tree.Insert(1);
+tree.Insert(6);
+tree.Insert(15);
+tree.Insert(170);
+
+
+Console.WriteLine(tree.GetFormattedJson());
+
+Console.WriteLine(tree.LookupIterative(170));
+Console.WriteLine(tree.LookupIterative(171));
